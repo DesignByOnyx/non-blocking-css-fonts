24fc0d1169c24877eb6970a7d6def00dc9119a2a
Ryan Wheale <ryan.wheale@gmail.com>
2013-10-05 15:58:52 -0600

    Updated code to test if font files have loaded

--- README.md
+++ README.md
@@ -1,30 +1,27 @@
 #Non-Blocking CSS Fonts#
 
-This plugin was created to allow for quicker loading webpages while still enjoying all of the benefits of web fonts.  You will need the CSS styles in order for this to work properly.
+**NOTE: This branch includes some extra code to determine when the font files have finished loading.  The code was inspired by [this Stack Overflow answer](http://stackoverflow.com/questions/4383226/using-jquery-to-know-when-font-face-fonts-are-loaded#answer-11689060).**
+
+This plugin was created to allow for quicker loading webpages while still enjoying all of the benefits of web fonts.  For example, the [Google font loader](http://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js) is 17K (7K gzip) while this is less than 1K.
 
 **You can read about it soon on my upcoming blog.**
 
-##Initial setup
+##Setup
 
-- Your `@font-face` styles should be separate from all other styles (this is just general good practice)
-- Remove any existing `<link>` tags which are currently loading your font stylesheets... but save the URL!
+- Your `@font-face` styles should be separate from all other styles (this is just general good practice).
+- Remove any existing `<link>` tags which are currently loading your font stylesheets.
+- Place this javascript in the `<head>` of your document - **it MUST go in the `<head>`**
 - Replace the long string at the bottom of this code with the URL to your CSS fonts file
-- Place this code in the `<head>` of your document - **it MUST go in the `<head>`**
-    - you can paste it right in 
-    - ...or you can load it via a `<script>` tag.  I personally load Modernizr in the `<head>`, so I just include this inside my Modernizr file.
-- This code MUST go in the `<head>` of your document!
 
 ##How does it work?
 
-We wait until the DOM is ready before loading our CSS fonts.  
+We wait until the DOM is ready before loading the CSS fonts.  
 
 ###But doesn't that cause FOUT?
 
-In order to prevent any flashing of unstyled content, we hide our text until the fonts have loaded.  I **highly recommend** showing as **much** as you can while hiding the text (show any backgrounds, borders, logos, structural styles).  This gives the *feeling* of a faster loading page - much better than a blank white page.  
+In order to prevent any flashing of unstyled text, we hide our text until the fonts have loaded.  I highly recommend **showing as much as you can** while hiding the text (show any backgrounds, borders, logos, structural styles, etc).  This gives the *feeling* of a faster loading page - much better than a blank white page.  
 
 ##Some handy information
 
-1. If the fonts take longer than ~500ms to load, we cancel the font loading (by removing the `<link>` tag we inserted) and display the page without any custom fonts.  This seeks to keep the user from waiting for content.  Feel free to adjust this "fail-safe" timeout to suit your needs.  I recommend not going much over 1000ms.  Remember, we are trying to stay fast.  
+1. If the fonts take longer than ~500ms to load, we cancel the font loading (by removing the `<link>` tag we inserted) which causes the page to render without any custom fonts.  This seeks to keep the user from waiting for content.  Feel free to adjust this "fail-safe" timeout to suit your needs.  I recommend not going much over 1000ms.  Remember, we are trying to stay fast.  
 **NOTE:** The CSS file should continue to load in the background.  If your caching is right, then all subsequent page views will load the CSS from cache.
-
-2. Once the CSS file has loaded, the browser still needs to download the actual font files (.woff, .ttf, etc).  The only *accurate* way to know when these files have truly finished loading is to wait for the `window.load` event to fire, but this means we are also waiting for every `object`, `img`, and `script` tag to finish loading before we show any text - this is BAD UX.  Instead, we estimate the time it should take for the font files to load (in my tests, ~100ms for google fonts) and then show the text.  HOWEVER, this ***can*** result in FOUT when the internet is slow.  Feel free to adjust this timeout to suit your needs.  You should also check out [this really cool solution on Stack Overflow](http://stackoverflow.com/questions/4383226/using-jquery-to-know-when-font-face-fonts-are-loaded#answer-11689060), which may eventually make it's way into this library.
--- non-blocking-css-fonts.js
+++ non-blocking-css-fonts.js
@@ -1,4 +1,4 @@
-(function(document, fontString) {
+(function(document, fontCSS, fontNames) {
 	"use strict";
 	var timer = 0,
 		link = null,
@@ -7,42 +7,97 @@
 		attachEvent = 'attachEvent',
 		addEventListener = 'addEventListener',
 		removeEventListener = 'removeEventListener',
-		readyEvent = 'DOMContentLoaded',
-		//loadEvent = 'load',
-		injectCss = function(){
-			link = document.createElement("link");
-			link.type = "text/css";
-			link.rel = "stylesheet";
-			link.href = fontString;
-			
-			// unbind DOM ready event, bind CSS load event
-			if( addEventListener ) {
-				document[removeEventListener](readyEvent, injectCss, false);
-				link[addEventListener]('load', cssLoaded, false);
-			}
-			
-			head.appendChild(link);
-			
-			// fail-safe timer in case the font provider is slow
-			timer = setTimeout(cssLoaded, 500);
-		},
-		cssLoaded = function(ev) {
-			// if there is NO event object, the fail-safe timer fired, remove css
-			if( !ev ) head.removeChild(link); // 
-			clearTimeout(timer);
-			
-			// We need to give time for the actual font files to load
-			//if(addEventListener) window[addEventListener](loadEvent, showText, false);
-			timer = setTimeout(showText, 100);
-		},
-		showText = function(ev) {
-			clearTimeout(timer);
-			
-			//if(addEventListener) window[removeEventListener](loadEvent, showText, false);
-			// Set a class on the HTML tag to show the text.
-			html.setAttribute('class', html.getAttribute('class').replace('fonts-loading', 'fonts-loaded'));
-			link = head = html = null;
-		};
+		readyEvent = 'DOMContentLoaded';
+		//loadEvent = 'load';
+		
+	function injectCss(){
+		link = document.createElement("link");
+		link.type = "text/css";
+		link.rel = "stylesheet";
+		link.href = fontCSS;
+		
+		// unbind DOM ready event, bind CSS load event
+		if( addEventListener ) {
+			document[removeEventListener](readyEvent, injectCss, false);
+			link[addEventListener]('load', cssLoaded, false);
+		}
+		
+		head.appendChild(link);
+		
+		// fail-safe timer in case the font provider is slow
+		timer = setTimeout(cssLoaded, 500);
+	}
+	
+	function cssLoaded(ev) {
+		// if there is NO event object, the fail-safe timer fired, remove css
+		if( !ev ) head.removeChild(link); // 
+		clearTimeout(timer);
+		
+		// We need to give time for the actual font files to load
+		timer = setTimeout(showText, 500);
+		waitForWebfonts(showText);
+	}
+	
+	function waitForWebfonts(callback) {
+	    var nodes = {},
+	    	length = "length",
+	    	style = "style",
+	    	normal = "normal";
+	    	
+	    for(var i = 0, l = fontNames[length]; i < l; ++i) {
+	    	var node = document.createElement('span');
+	    	
+	    	// Characters that vary significantly among different fonts
+	        node.innerHTML = 'giItT1WQy@!-/#';
+	        
+	        // Visible - so we can measure it - but not on the screen
+	        node[style].position      = 'absolute';
+	        node[style].top           = '-1000em';
+	        
+	        // Large font size makes even subtle changes obvious
+	        node[style].fontSize      = '300px';
+	        
+	        // Reset any font properties
+	        node[style].fontFamily    = 'serif';
+	        node[style].fontVariant   = normal;
+	        node[style].fontStyle     = normal;
+	        node[style].fontWeight    = normal;
+	        node[style].letterSpacing = 0;
+	        document.body.appendChild(node);
+	        
+	        // Remember width with no applied web font
+	        nodes[node.offsetWidth] = node;
+	        
+	        // Now apply the font family to the node
+	        node[style].fontFamily = fontNames[i];
+	    }
+	    
+	    (function checkNodes() {
+	    	var width;
+		    for(width in nodes) {
+			    if( width == nodes[width].offsetWidth ) {
+			    	// one of the fonts has not loaded.  Wait a little longer
+				    setTimeout(checkNodes, 50);
+				    return;
+			    } else {
+			    	// The font has loaded.  Clean things up.
+				    nodes[width].parentNode.removeChild(nodes[width]);
+				    delete nodes[width];
+			    }
+		    }
+		    // Pass "true" so that we know the fail-safe timer didn't fire.
+		    callback(true);
+	    }());
+	}
+	
+	function showText(ev) {
+		if( !ev ) head.removeChild(link);
+		clearTimeout(timer);
+		
+		// Set a class on the HTML tag to show the text.
+		html.setAttribute('class', html.getAttribute('class').replace('fonts-loading', 'fonts-loaded'));
+		link = head = html = null;
+	};
 	
 	// Set a class on the HTML tag while fonts are loading 
 	// Use CSS to hide the text on the page
@@ -56,8 +111,6 @@
 			&& (removeEventListener = 'detachEvent')
 			&& attachEvent : '';
 	
-	// use the following if you want this globally available
-	//(window.domReady = function(f) {
 	(function domReady(f) { 
 		/in/.test(document.readyState) ? 
 			!addEventListener ? 
@@ -65,4 +118,4 @@
 				: document[addEventListener](readyEvent, f, false)
 			: f(); 
 	}(injectCss));
-}(document, 'http://fonts.googleapis.com/css?family=Parisienne'));
+}(document, 'http://fonts.googleapis.com/css?family=Parisienne', ['Parisienne']));
